04c84593b5ab1e885bc9bb6284e2cb7851fd8663
илья <asredred@gmail.com>
2025-05-25 09:28:20 +0300

    back

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import mysql from 'mysql2/promise';
-import img from './'
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
